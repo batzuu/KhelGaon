eab87038217d72c94b70cf7395dd560009728bce
batzuu <utkarsh1616@gmail.com>
2023-04-12 12:16:19 +0530

    lesgo

---
+++ client/src/pages/PlacePage.jsx
@@ -0,0 +1,85 @@
+import { Link, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import BookingWidget from "../BookingWidget";
+import PlaceGallery from "../PlaceGallery";
+import AddressLink from "../AddressLink";
+import Perks from "../Perks";
+
+export default function PlacePage() {
+	const { id } = useParams();
+	const [place, setPlace] = useState(null);
+	const [title, setTitle] = useState("");
+	const [address, setAddress] = useState("");
+	const [addedPhotos, setAddedPhotos] = useState([]);
+	const [description, setDescription] = useState("");
+	const [perks, setPerks] = useState([]);
+	const [extraInfo, setExtraInfo] = useState("");
+	const [checkIn, setCheckIn] = useState("");
+	const [checkOut, setCheckOut] = useState("");
+	const [maxGuests, setMaxGuests] = useState(1);
+	const [price, setPrice] = useState(100);
+	const [redirect, setRedirect] = useState(false);
+	useEffect(() => {
+		if (!id) {
+			return;
+		}
+		axios.get("/places/" + id).then((response) => {
+			const { data } = response;
+			setTitle(data.title);
+			setAddress(data.address);
+			setAddedPhotos(data.photos);
+			setDescription(data.description);
+			setPerks(data.perks);
+			setExtraInfo(data.extraInfo);
+			setCheckIn(data.checkIn);
+			setCheckOut(data.checkOut);
+			setMaxGuests(data.maxGuests);
+			setPrice(data.price);
+		});
+	}, [id]);
+	useEffect(() => {
+		if (!id) {
+			return;
+		}
+		axios.get(`/places/${id}`).then((response) => {
+			setPlace(response.data);
+		});
+	}, [id]);
+
+	if (!place) return "";
+
+	return (
+		<div className="mt-4 bg-gray-100 -mx-8 px-8 pt-8">
+			<h1 className="text-3xl">{place.title}</h1>
+			<AddressLink>{place.address}</AddressLink>
+			<PlaceGallery place={place} />
+			<div className="mt-8 mb-8 grid gap-8 grid-cols-1 md:grid-cols-[2fr_1fr]">
+				<div>
+					<div className="my-4">
+						<h2 className="font-semibold text-2xl">Description</h2>
+						{place.description}
+					</div>
+					Check-in: {place.checkIn}
+					<br />
+					Check-out: {place.checkOut}
+					<br />
+					Max number of players: {place.maxGuests}
+				</div>
+				<div>
+					<BookingWidget place={place} />
+				</div>
+			</div>
+
+			<Perks selected={perks} onChange={setPerks} />
+			<div className="bg-white -mx-8 px-8 py-8 border-t">
+				<div>
+					<h2 className="font-semibold text-2xl">Extra info</h2>
+				</div>
+				<div className="mb-4 mt-2 text-sm text-gray-700 leading-5">
+					{place.extraInfo}
+				</div>
+			</div>
+		</div>
+	);
+}
